cc74aa9435f29279cc7c0861749a89d7e8afae60
jenny <88226911+kmjenny@users.noreply.github.com>
2024-02-15 00:20:10 +0900

    [init] Font, Style 추가

---
+++ styles/color_styles.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ColorStyles {
+  // main
+  static const Color main1 = Color(0xFF377B87);
+  static const Color main2 = Color(0xFFC0CEBB);
+
+  // sub
+  static const Color sub1 = Color(0xFF647D5D);
+  static const Color sub2 = Color(0xFFC1DBDB);
+  static const Color sub3 = Color(0xFF22383C);
+
+  // black, white
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+
+  // gray
+  static const Color grey1 = Color(0xFFCBC8C5);
+  static const Color grey2 = Color(0xFF878784);
+  static const Color grey3 = Color(0xFFE2E2E2);
+
+  // error
+  static const Color error = Color(0xFFFF2633);
+
+  // background
+  static const Color background = Color(0xFFF8F2EB);
+
+  // topbar
+  static const Color topbar = Color(0xFF606160);
+}
